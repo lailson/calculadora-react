4d2056d71cf72b6cb331b15b2bd17bc7a4a57952
lailson henrique <lailson.pi@gmail.com>
2020-06-01 23:28:49 -0300

    Updates

---
+++ precache-manifest.8eebb748b8d30148dc213db768075701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0044c156582356119340a21cd8f9efe9",
+    "url": "/calculadora-react/index.html"
+  },
+  {
+    "revision": "48a07138a7c6345452ce",
+    "url": "/calculadora-react/static/css/main.b9f6e00e.chunk.css"
+  },
+  {
+    "revision": "40e197f63389d1b06622",
+    "url": "/calculadora-react/static/js/2.fa6a5fe9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculadora-react/static/js/2.fa6a5fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a07138a7c6345452ce",
+    "url": "/calculadora-react/static/js/main.129fe045.chunk.js"
+  },
+  {
+    "revision": "f061849caed948c9d5f3",
+    "url": "/calculadora-react/static/js/runtime-main.2c3031d8.js"
+  }
+]);
