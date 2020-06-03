4c50c258954d75c04b91b576b342372c34724d01
lailson henrique <lailson.pi@gmail.com>
2020-06-03 01:09:24 -0300

    teste unitario - subtrair multiplicar dividir ponto e pontos

--- src/Calculadora.test.js
+++ src/Calculadora.test.js
@@ -20,5 +20,57 @@ describe('Calculadora', () => {
         fireEvent.click(getByText('='))
         expect(getByTestId('txtNumeros').textContent).toEqual('5')
     })
+
+    it('deve subtrair  5 - 3 e obter 2', () => {
+        const { getByTestId, getByText } = render(<Calculadora />)
+        fireEvent.click(getByText('5'))
+        fireEvent.click(getByText('-'))
+        fireEvent.click(getByText('3'))
+        fireEvent.click(getByText('='))
+        expect(getByTestId('txtNumeros').textContent).toEqual('2')
+    })
+
+    it('deve dividir  6 - 3 e obter 2', () => {
+        const { getByTestId, getByText } = render(<Calculadora />)
+        fireEvent.click(getByText('6'))
+        fireEvent.click(getByText('/'))
+        fireEvent.click(getByText('3'))
+        fireEvent.click(getByText('='))
+        expect(getByTestId('txtNumeros').textContent).toEqual('2')
+    })
+
+    it('deve multiplicar  2 x 3 e obter 6', () => {
+        const { getByTestId, getByText } = render(<Calculadora />)
+        fireEvent.click(getByText('2'))
+        fireEvent.click(getByText('X'))
+        fireEvent.click(getByText('3'))
+        fireEvent.click(getByText('='))
+        expect(getByTestId('txtNumeros').textContent).toEqual('6')
+    })
+
+    it('deve somar  2.5 + 3 e obter 5.5', () => {
+        const { getByTestId, getByText } = render(<Calculadora />)
+        fireEvent.click(getByText('2'))
+        fireEvent.click(getByText('.'))
+        fireEvent.click(getByText('5'))
+        fireEvent.click(getByText('+'))
+        fireEvent.click(getByText('3'))
+        fireEvent.click(getByText('='))
+        expect(getByTestId('txtNumeros').textContent).toEqual('5.5')
+    })
+
+    it('deve somar  2.52 + 3 e obter 5.5 apertando mais de uma vez no ponto', () => {
+        const { getByTestId, getByText } = render(<Calculadora />)
+        fireEvent.click(getByText('2'))
+        fireEvent.click(getByText('.'))
+        fireEvent.click(getByText('5'))
+        fireEvent.click(getByText('.'))
+        fireEvent.click(getByText('2'))
+        fireEvent.click(getByText('+'))
+        fireEvent.click(getByText('3'))
+        fireEvent.click(getByText('='))
+        expect(getByTestId('txtNumeros').textContent).toEqual('5.52')
+    })
+    
 })
 
